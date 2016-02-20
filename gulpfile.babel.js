2c11b95824b9c9bb731b2038b2ac0deea581d564
Bruno Dias <bruno.r.dias@gmail.com>
2016-02-20 03:58:00 -0200

    Prep for npm publishing

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,17 @@
 import gulp from 'gulp';
 import mocha from 'gulp-mocha';
+import babel from 'gulp-babel';
 
-function test() {
+function test () {
   return gulp.src('test/*.js')
     .pipe(mocha());
 }
 
+function prepublish () {
+  return gulp.src('lib/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dist'));
+}
+
 gulp.task('default', test);
+gulp.task('prepublish', prepublish);
